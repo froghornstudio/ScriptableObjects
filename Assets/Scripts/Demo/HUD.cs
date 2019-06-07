6c23aa572a83f289a72546c856057d09620b8bfc
froghornstudio <49729018+froghornstudio@users.noreply.github.com>
2019-06-07 02:39:59 -0400

    initial commit

---
+++ Assets/Scripts/Demo/HUD.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUD : MonoBehaviour
+{
+    public PlayerRunTimeInfo PlayerRunTimeInfo;
+
+    private Text text;
+
+    private void Awake() {
+        text = GetComponent<Text>();
+    }
+
+    void Update() {
+        text.text = PlayerRunTimeInfo.player1_current_health.ToString() + "\n";
+        text.text += PlayerRunTimeInfo.player1_max_health.ToString() + "\n";
+        text.text += PlayerRunTimeInfo.player2_current_health.ToString() + "\n";
+        text.text += PlayerRunTimeInfo.player2_max_health.ToString() + "\n";
+    }
+}
---
+++ Assets/Scripts/GameState/PlayerRunTimeInfo.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//[CreateAssetMenu]
+public class PlayerRunTimeInfo : ScriptableObject
+{
+
+    [Header("Runtime Game Values")]
+
+    //Current Save Slot
+    public int current_save_slot;
+
+    //Player1
+    public int player1_current_health;
+    public int player1_max_health;
+    //Player2
+    public int player2_current_health;
+    public int player2_max_health;
+
+}
